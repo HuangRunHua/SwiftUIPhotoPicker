c879febaf54fa35ce1a195f7022c5ef016fe2f12
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-04-02 13:52:33 +0800

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Runhua Huang on 2022/4/2.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
