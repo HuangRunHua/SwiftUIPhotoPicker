b3d72c3a5f2d0ba3ae7fe8f3e33646945bb7dd24
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-04-02 15:25:42 +0800

    Create Persistence.swift

---
+++ PhotoPicker/Persistence.swift
@@ -0,0 +1,46 @@
+//
+//  Persistence.swift
+//  PhotoPicker
+//
+//  Created by Runhua Huang on 2022/4/2.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    // 这一部分用于Xcode预览功能
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        for _ in 0..<1 {
+            let newItem = ImageModel(context: viewContext)
+            newItem.id = UUID()
+            newItem.imageData = Data()
+        }
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "ImageModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+}
